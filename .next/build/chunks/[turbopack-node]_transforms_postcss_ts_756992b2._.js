c25f7f2d3ef2e04db1201d65d9027636bd992ea9
imadmlf <imadmaalouf02@gmail.com>
2025-11-30 15:41:51 +0100

    cc

---
+++ .next/build/chunks/[turbopack-node]_transforms_postcss_ts_756992b2._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Documents/these/infodoc/react-app-setup/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/a7454_1cd22a79._.js",
+  "chunks/[root-of-the-server]__b7a2917c._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Documents/these/infodoc/react-app-setup/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
